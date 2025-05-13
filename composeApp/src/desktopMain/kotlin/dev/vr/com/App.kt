f9c944a445c9de8a99062b5d4185b9dd1cf98960
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-05-13 19:49:17 +0300

    update background

--- composeApp/src/desktopMain/kotlin/dev/vr/com/App.kt
+++ composeApp/src/desktopMain/kotlin/dev/vr/com/App.kt
@@ -1,17 +1,15 @@
 package dev.vr.com
 
-import Carousel
 import androidx.compose.animation.slideInHorizontally
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
+import dev.vr.com.core.AnimatedGradientCirclesBackground
 import dev.vr.com.core.Theme
 import dev.vr.com.core.VRTheme
 import dev.vr.com.core.components.RoundedButton
@@ -20,7 +18,8 @@ import dev.vr.com.presentation.ArenaScreen
 import dev.vr.com.presentation.HolidaysScreen
 import dev.vr.com.presentation.ZoneScreen
 import org.jetbrains.compose.resources.painterResource
-import vr.composeapp.generated.resources.*
+import vr.composeapp.generated.resources.Res
+import vr.composeapp.generated.resources.`Обложка 10`
 
 @Composable
 fun App() {
@@ -28,84 +27,89 @@ fun App() {
     val navController = rememberNavController()
 
     VRTheme {
-        Column (
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Theme.colors.primaryBackground)
-        ) {
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                RoundedButton(
-                    text = "VR ARENA",
-                    color = Theme.colors.blueAction,
-                ) {
-                    navController.navigate(Route.Arena)
-                }
+        Box(modifier = Modifier.fillMaxSize()) {
+            AnimatedGradientCirclesBackground()
 
-                RoundedButton(
-                    text = "VR ZONE",
-                    color = Theme.colors.pinkAction,
-                ) {
-                    navController.navigate(Route.Zone)
-                }
-
-                RoundedButton(
-                    text = "ПРАЗДНИКИ И МЕРОПРИЯТИЯ",
-                    color = Theme.colors.blueAction,
-                ) {
-                    navController.navigate(Route.Holidays)
-                }
-
-                RoundedButton(
-                    text = "НАСТРОЙКИ",
-                    color = Theme.colors.pinkAction,
-                ) {
-
-                }
-            }
-
-            Image(
-                modifier = Modifier
-                    .fillMaxWidth(1f),
-                painter = painterResource(Res.drawable.`Обложка 10`),
-                contentDescription = "main image"
-            )
-
-            NavHost(
-                navController = navController,
-                startDestination = Route.Arena,
+            Column(
                 modifier = Modifier
                     .fillMaxSize()
+//                    .background(Theme.colors.primaryBackground)
             ) {
 
-                composable<Route.Zone>(
-                    exitTransition = { slideOutHorizontally() },
-                    popEnterTransition = { slideInHorizontally() }
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
                 ) {
-                    ZoneScreen(
-                        navController = navController
-                    )
+                    RoundedButton(
+                        text = "VR ARENA",
+                        color = Theme.colors.blueAction,
+                    ) {
+                        navController.navigate(Route.Arena)
+                    }
+
+                    RoundedButton(
+                        text = "VR ZONE",
+                        color = Theme.colors.pinkAction,
+                    ) {
+                        navController.navigate(Route.Zone)
+                    }
+
+                    RoundedButton(
+                        text = "ПРАЗДНИКИ И МЕРОПРИЯТИЯ",
+                        color = Theme.colors.blueAction,
+                    ) {
+                        navController.navigate(Route.Holidays)
+                    }
+
+                    RoundedButton(
+                        text = "НАСТРОЙКИ",
+                        color = Theme.colors.pinkAction,
+                    ) {
+
+                    }
                 }
 
-                composable<Route.Arena>(
-                    exitTransition = { slideOutHorizontally() },
-                    popEnterTransition = { slideInHorizontally() }
-                ) {
-                    ArenaScreen(
-                        navController = navController
-                    )
-                }
+                Image(
+                    modifier = Modifier
+                        .fillMaxWidth(1f),
+                    painter = painterResource(Res.drawable.`Обложка 10`),
+                    contentDescription = "main image"
+                )
 
-                composable<Route.Holidays>(
-                    exitTransition = { slideOutHorizontally() },
-                    popEnterTransition = { slideInHorizontally() }
+                NavHost(
+                    navController = navController,
+                    startDestination = Route.Arena,
+                    modifier = Modifier
+                        .fillMaxSize()
                 ) {
-                    HolidaysScreen(
-                        navController = navController
-                    )
+
+                    composable<Route.Zone>(
+                        exitTransition = { slideOutHorizontally() },
+                        popEnterTransition = { slideInHorizontally() }
+                    ) {
+                        ZoneScreen(
+                            navController = navController
+                        )
+                    }
+
+                    composable<Route.Arena>(
+                        exitTransition = { slideOutHorizontally() },
+                        popEnterTransition = { slideInHorizontally() }
+                    ) {
+                        ArenaScreen(
+                            navController = navController
+                        )
+                    }
+
+                    composable<Route.Holidays>(
+                        exitTransition = { slideOutHorizontally() },
+                        popEnterTransition = { slideInHorizontally() }
+                    ) {
+                        HolidaysScreen(
+                            navController = navController
+                        )
+                    }
                 }
             }
         }
