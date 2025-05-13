1742b2588c7b38a3bcae8a61987bfe5448c97fed
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-05-13 19:11:52 +0300

    update logic

--- composeApp/src/desktopMain/kotlin/dev/vr/com/presentation/HolidaysScreen.kt
+++ composeApp/src/desktopMain/kotlin/dev/vr/com/presentation/HolidaysScreen.kt
@@ -2,11 +2,12 @@ package dev.vr.com.presentation
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.navigation.NavController
 
 @Composable
 fun HolidaysScreen(
     navController: NavController
 ) {
-    Text("HOLIDAYS")
+    Text("HOLIDAYS", color = White)
 }
--- composeApp/src/desktopMain/kotlin/dev/vr/com/presentation/ZoneScreen.kt
+++ composeApp/src/desktopMain/kotlin/dev/vr/com/presentation/ZoneScreen.kt
@@ -1,17 +1,13 @@
 package dev.vr.com.presentation
 
-import Carousel
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.navigation.NavController
-import vr.composeapp.generated.resources.Res
-import vr.composeapp.generated.resources.img1
-import vr.composeapp.generated.resources.img2
-import vr.composeapp.generated.resources.img3
 
 @Composable
 fun ZoneScreen(
     navController: NavController
 ) {
-    Text("ZONE")
+    Text("ZONE", color = White)
 }
