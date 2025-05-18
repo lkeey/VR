7dfa8b247593522f42bce07447ecdb6334eff637
Aleksey Kiryushin <97094952+lkeey@users.noreply.github.com>
2025-05-18 19:12:55 +0300

    update jdk

--- composeApp/src/desktopMain/kotlin/dev/vr/com/core/components/VideoPopUp.kt
+++ composeApp/src/desktopMain/kotlin/dev/vr/com/core/components/VideoPopUp.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -27,12 +28,24 @@ fun VideoPopUp(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                VideoPlayer(
+//                VideoPlayer(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .height(400.dp),
+//                    url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+//                )
+                Player(
+                    url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                    onFinish = {},
                     modifier = Modifier
                         .fillMaxWidth()
                         .height(400.dp),
-//                    url = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                    url = "drawable/movie.mp4"
+                    isResumed = true,
+                    volume = 1f,
+                    speed = 1f,
+                    seek = 0f,
+                    isFullscreen = false,
+                    progressState = mutableStateOf( Progress(0f, 0))
                 )
 
 //                DesktopVideoPlayer(
